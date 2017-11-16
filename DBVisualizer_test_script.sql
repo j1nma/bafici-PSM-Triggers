68502e3757f73c3a8f5ebce428404dfa45a63dfc
j1nma <j1nma@users.noreply.github.com>
2017-11-16 18:28:52 -0300

    REGEX filter for all whitespace characters. DBV working script.

--- DBVisualizer_test_script.sql
+++ DBVisualizer_test_script.sql
@@ -1,3 +1,11 @@
+SELECT inserta_datos_peliculas();
+
+SELECT inserta_datos_dirige();
+
+SELECT inserta_datos_pertenece();
+
+SELECT inserta_datos_actor();
+
 CREATE OR REPLACE FUNCTION inserta_datos_actor() RETURNS VOID
 AS $$
 
