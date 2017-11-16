121615a7c8bc1e7c5087c7004a7911b06fad9a71
j1nma <j1nma@users.noreply.github.com>
2017-11-16 20:31:30 -0300

    Trying to automize column name filter.

--- functions3.sql
+++ functions3.sql
@@ -30,16 +30,43 @@ countries CURSOR FOR
 	);
 country RECORD;
 
+--JMA
+countryIDs CURSOR FOR (SELECT COLUMN_NAME
+							FROM INFORMATION_SCHEMA.COLUMNS
+							WHERE COLUMN_NAME LIKE 'id_country%' AND TABLE_NAME = 'film');
+countryID RECORD;
+
+--JMA
+
 BEGIN
-	OPEN countries;
+	-- OPEN countries;
+	-- LOOP
+ --    FETCH countries INTO country;
+ --        EXIT WHEN NOT FOUND;
+ --        IF (country.id_country <> 0) THEN
+ --        	INSERT INTO pertenece 
+ --        	VALUES (country.id_film, country.id_country)
+ --        	ON CONFLICT DO NOTHING;
+ --        END IF;
+ --    END LOOP;
+
+ --JMA
+ OPEN countryIDs;
 	LOOP
-    FETCH countries INTO country;
+    FETCH countryIDs INTO countryID;
         EXIT WHEN NOT FOUND;
-        IF (country.id_country <> 0)
+
+        SELECT id_country, id_film
+		FROM film
+		WHERE film.countryID;
+
+        IF (countryID.id_country <> 0) THEN
         	INSERT INTO pertenece 
-        	VALUES (country.id_film, country.id_country)
+        	VALUES (countryID.id_film, countryID.id_country)
         	ON CONFLICT DO NOTHING;
         END IF;
     END LOOP;
+--JMA
+
 END;
 $$ LANGUAGE plpgSQL;
