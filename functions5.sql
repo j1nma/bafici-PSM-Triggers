d1adf5723f81c068a30eeac4afb7fba80b0703ae
Ioni Nielavitzky <jnielavitzky@itba.edu.ar>
2017-11-16 21:43:46 -0300

    Add actua ya funciona. Trigger no updateea cant_films.

---
+++ functions5.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION inserta_datos_actua() RETURNS VOID
+AS $$
+
+
+DECLARE
+actores CURSOR FOR SELECT id_actor, nombre
+FROM actor;
+actor RECORD;
+pelicula RECORD;
+
+BEGIN
+OPEN actores;
+	LOOP
+	FETCH actores INTO actor;
+	EXIT WHEN NOT FOUND;
+
+		FOR pelicula IN
+			(SELECT id_film
+			FROM film
+			WHERE REGEXP_REPLACE(REGEXP_REPLACE(LOWER(film.cast_), '^\s+', ''), '\s+$', '') LIKE ('%'|| actor.nombre ||'%'))
+		LOOP
+			INSERT INTO actua
+			VALUES (actor.id_actor, pelicula.id_film)
+			ON CONFLICT DO NOTHING;
+		END LOOP;
+	END LOOP;
+END;
+$$ LANGUAGE plpgSQL;
